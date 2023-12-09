cdb0d64e3ab4d37a0c3b453925d3e12c2717cf5e
alopvaz <alopvaz014@iesalmudeyne.es>
2023-12-09 23:28:10 +0100

    hola

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main (String [] args) {
-        System.out.println("Hola mundo main 1");
+        System.out.println("Hola mundo main 1 por segunda vez");
     }
 }
